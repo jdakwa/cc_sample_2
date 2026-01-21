cd403ac86234780493d75e56772956afb5607e5a
Julian Dakwa <juliandakwa@yahoo.com>
2026-01-21 09:25:43 -0800

    change photo

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -21,7 +21,7 @@ export default function AboutSection() {
           <ScrollAnimation animationType="fade-in-left">
             <div className="relative h-96 lg:h-[500px] rounded-lg overflow-hidden shadow-2xl">
               <Image
-                src="https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=800&q=80"
+                src="https://images.unsplash.com/photo-1718209881007-c0ecdfc00f9d?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Luxury real estate"
                 fill
                 className="object-cover"
